e2aa2566b61314d18c32a57e09caa0aa3f236e53
Eric Dockter <Eric Dockter>
2017-01-30 23:03:13 -0500

    Completed lecture demo.

--- homeinventory/app/models/item.rb
+++ homeinventory/app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ActiveRecord::Base
+    def foo 
+        puts "foo"
+    end
+
+    def addUp(n1, n2)
+        n1 + n2
+    end
+
+    def age
+        Date.today.year - self.purchase_date.year
+    end
+
 end
