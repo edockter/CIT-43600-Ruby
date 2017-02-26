d8e055068037d99e0a3e85551fd60c9633482954
Eric Dockter <ericdock@iupui.edu>
2017-02-26 03:16:12 -0500

    Finished assignment, now need a little more boootsrappification. Added FontAwesome (for fun)

--- campusevents/app/models/attendee.rb
+++ campusevents/app/models/attendee.rb
@@ -3,4 +3,8 @@ class Attendee < ActiveRecord::Base
 
     has_many :subscriptions
     has_many :events, :through => :subscriptions
+
+    def full_name
+        ret = first_name + " " +  last_name
+    end
 end
