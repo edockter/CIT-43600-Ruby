d6b278f8d8bae43313bcb1499b37ca7ce733b52d
Eric Dockter <ericdock@iupui.edu>
2017-03-01 23:10:21 -0500

    Completed Campus Events, pt. 4 assignment

--- campusevents/app/models/event.rb
+++ campusevents/app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ActiveRecord::Base
     validates :name, :start_date, :end_date, presence: true
-    validate :start_is_before_end
 
     has_and_belongs_to_many :sponsors
     has_many :meetings
@@ -9,9 +8,4 @@ class Event < ActiveRecord::Base
 
 
 
-    def start_is_before_end
-        if end_date < start_date
-            errors.add :end_date, "- event must start before it can end."
-        end
-    end
 end
